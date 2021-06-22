b7fee8a654708ac45454fb6baa8cdef10672c9f9
Al-Ahmadgaid Asaad <al-ahmadgaidasaad@192.168.254.104>
2021-06-23 00:27:21 +0800

    added install_camel

--- src/data.jl
+++ src/data.jl
@@ -1,9 +1,9 @@
 function install_camel(full_data::Bool=false)
     if full_data
-        run(`pip install camel-tools`)
+        run(`pip3 install camel-tools`)
         run(`camel_data full`)
     else
-        run(`pip install camel-tools`)
+        run(`pip3 install camel-tools`)
         run(`camel_data light`)
     end
 end
