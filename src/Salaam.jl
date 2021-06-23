0f3a7242d7d18c12402691465ce5475dca490e56
Al-Ahmadgaid Asaad <al-ahmadgaidasaad@192.168.254.104>
2021-06-23 09:23:29 +0800

    organized into folders

--- src/Salaam.jl
+++ src/Salaam.jl
@@ -4,20 +4,20 @@ using PyCall: pyimport, PyObject
 abstract type AbstractModel end
 
 include("constants.jl")
-include("transliterate.jl")
+include("transliterator/transliterate.jl")
 include("decode.jl")
 include("normalize.jl")
 include("dediac.jl")
 include("encode.jl")
-include("disambig.jl")
-include("database.jl")
-include("analyze.jl")
-include("generate.jl")
-include("reinflect.jl")
-include("tag.jl")
-include("tokenize.jl")
-include("identify_dialect.jl")
-include("data.jl")
+include("disambig/disambig.jl")
+include("data/database.jl")
+include("morphology/analyze.jl")
+include("morphology/generate.jl")
+include("morphology/reinflect.jl")
+include("tagger/tag.jl")
+include("tokenizers/tokenize.jl")
+include("dialectid/identify_dialect.jl")
+include("data/data.jl")
 
 export BW_ENCODING, AR_DIACS_REGEX, SP_REGEX_CHARS, PUNCTUATIONS_REGEX
 
--- src/data/data.jl
+++ src/data/data.jl
@@ -1,9 +1,13 @@
 function install_camel(full_data::Bool=false)
     if full_data
+        @info "installing camel-tools"
         run(`pip3 install camel-tools`)
+        @info "downloading camel_data"
         run(`camel_data full`)
     else
+        @info "installing camel-tools"
         run(`pip3 install camel-tools`)
+        @info "downloading camel_data"
         run(`camel_data light`)
     end
 end
