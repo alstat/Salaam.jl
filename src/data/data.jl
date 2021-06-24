0f05f5f47a87ec7d3d34bf76154adba8252cf961
Al-Ahmadgaid Asaad <al-ahmadgaidasaad@192.168.254.104>
2021-06-24 09:54:44 +0800

    updated installation

--- src/data/data.jl
+++ src/data/data.jl
@@ -8,6 +8,7 @@ function install_camel(full_data::Bool=false, data_directory::Union{Nothing,Stri
         else
             @info "downloading camel_data to " * data_directory
             ENV["CAMELTOOLS_DATA"] = data_directory
+            run(`export $(ENV["CAMELTOOLS_DATA"])`)
             run(`camel_data full`)
         end
     else
@@ -19,6 +20,7 @@ function install_camel(full_data::Bool=false, data_directory::Union{Nothing,Stri
         else
             @info "downloading camel_data to " * data_directory
             ENV["CAMELTOOLS_DATA"] = data_directory
+            run(`export $(ENV["CAMELTOOLS_DATA"])`)
             run(`camel_data light`)
         end
     end
